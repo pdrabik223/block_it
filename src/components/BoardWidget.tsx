4dc58195bbabf457afd14e060b76c63a05019af3
Piotr Drabik <pdrabik223@gmail.com>
2025-10-03 05:53:22 +0200

    added border

--- src/components/BoardWidget.tsx
+++ src/components/BoardWidget.tsx
@@ -1,5 +1,5 @@
 import type { JSX } from 'react';
-import { Board } from '../engine/engine';
+import { Board, Cell } from '../engine/engine';
 import { CellWidget } from './CellWidget';
 import './BoardWidget.css'
 
@@ -8,10 +8,17 @@ interface BoardWidgetProps { board: Board }
 export const BoardWidget: React.FC<BoardWidgetProps> = (props: BoardWidgetProps) => {
 
     let data: JSX.Element[] = []
+    // add top border
+    let temp = [<CellWidget value={Cell.Red} />];
+    for (let x = 0; x < Board.height; x++) {
+        temp.push(<CellWidget value={Cell.Border} />);
+    }
+    temp.push(<CellWidget value={Cell.Blue} />)
+    data.push(<div className='row'>{temp}</div>);
 
     for (let x = 0; x < Board.height; x++) {
 
-        let temp = [];
+        let temp = [<CellWidget value={Cell.Border} />];
         for (let y = 0; y < Board.width; y++) {
 
             temp.push(
@@ -19,8 +26,17 @@ export const BoardWidget: React.FC<BoardWidgetProps> = (props: BoardWidgetProps)
                     <CellWidget value={props.board.get(x, y)} /></div>
             );
         }
+        temp.push(<CellWidget value={Cell.Border} />);
         data.push(<div className='row'>{temp}</div>);
+
     }
+    // add bottom border
+    temp = [<CellWidget value={Cell.Orange} />];
+    for (let x = 0; x < Board.height; x++) {
+        temp.push(<CellWidget value={Cell.Border} />);
+    }
+    temp.push(<CellWidget value={Cell.Green} />)
+    data.push(<div className='row'>{temp}</div>);
 
     return <div className='column'> {data} </div>;
 
