c912c41cc393093038f8a84ea35dd2b3e436e223
Piotr Drabik <pdrabik223@gmail.com>
2025-10-10 18:28:16 +0200

    returning to previous version

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/block_it/'
-  }
-
-  return config
+export default defineConfig({
+  plugins: [react()],
 })
